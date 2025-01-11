e0f00720f064d4efb36784035f37dc94f963c0e2
Manan Manchanda <mananmanchanda@gmail.com>
2025-01-11 23:18:06 +0530

    about us page

--- ecomm-frontend/src/components/NewsletterBox.tsx
+++ ecomm-frontend/src/components/NewsletterBox.tsx
@@ -12,9 +12,9 @@ const NewsletterBox = () => {
       </div>
       <p className="text-gray-400 mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet ea deserunt mollitia velit.</p>
 
-      <form onSubmit={onSubmithandler} className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3 rounded-md">
+      <form onSubmit={onSubmithandler} className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3">
         <input className="w-full sm:flex-1 outline-none" type="email" placeholder="Enter your email" required />
-        <button type="submit" className="bg-black text-white rounded-md px-4 py-2">SUBSCRIBE</button>
+        <button type="submit" className="bg-black text-white px-4 py-2">SUBSCRIBE</button>
       </form>
     </div>
   )
